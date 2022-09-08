f2f130dae8fe4fcfa8cd19678f41fc72d37e0f26
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2022-09-08 18:01:40 +0100

    Update ReverseString.java

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -6,7 +6,7 @@ public class ReverseString {
         System.out.println("input a string: ");
         char [] letters = reader.nextLine().toCharArray();
         System.out.println(letters);
-        for (char i = 0; i < letters.length; ++i){
+        for (int i = 0; i < letters.length; ++i){
             System.out.println(letters[i]);
         }
 
