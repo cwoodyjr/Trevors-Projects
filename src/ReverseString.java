0ecf36ab93ee9999613e345898d14db72707267b
Craig <craig.woodcock89@hotmail.co.uk>
2022-09-08 13:02:34 +0100

    Reverse String attempt 1

---
+++ src/ReverseString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("input a string: ");
+        char [] letters = reader.nextLine().toCharArray();
+        System.out.println(letters);
+        for (char i = 0; i < letters.length; ++i){
+            System.out.println(letters[i]);
+        }
+
+    }
+}
