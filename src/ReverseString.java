328e82b625117d2f44c8216598d096bc5138969b
Craig <craig.woodcock89@hotmail.co.uk>
2022-09-09 12:25:56 +0100

    Reverse String attempt 2, created string, converted to charArray, added for loop to print and reverse charArray

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,14 +1,30 @@
+//import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReverseString {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("input a string: ");
-        char [] letters = reader.nextLine().toCharArray();
-        System.out.println(letters);
-        for (int i = 0; i < letters.length; ++i){
-            System.out.println(letters[i]);
+        Scanner reader = new Scanner(System.in); //set up Scanner
+        System.out.println("input a string: "); //ask for input
+
+        String letters = reader.nextLine(); //convert input from Scanner to string
+        System.out.println("You entered "+letters); //print input
+
+        char [] resultArray = letters.toCharArray(); //convert input to character array
+        System.out.println("As a CharArray: ");
+        for (int i = 0; i != resultArray.length; i++){  //use a for loop to print character array
+                                                        //
+            System.out.print(resultArray[i]+" ");       //
         }
 
+        System.out.println(" ");                        // print a blank line to break up results
+
+        System.out.println("As a reverse CharArray: ");
+
+        for (int i = resultArray.length-1; i >= 0; i--){
+
+            System.out.print(resultArray[i]+" ");
+        }
+        System.out.println(" ");
+        System.out.println(letters);
     }
 }
